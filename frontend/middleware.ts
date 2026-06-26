8421db9400f818f169e14c532a451a7762239f93
sergiomusino <sergiomusino@gmail.com>
2026-06-26 23:24:20 +0200

    fix: semplificato middleware

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,10 +1,6 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export const config = {
-  matcher: [],
-};
-
 export async function middleware(request: NextRequest) {
   return NextResponse.next();
 }
